5074644df0723df1f8959ff3fe5aa5a863d25d61
Anass El Ouahabi <anasselouahabi2003@gmail.com>
2025-06-09 00:56:40 +0100

    fillierId to int

---
+++ backend/services/AuthService/Migrations/20250608234223_FiliereIdToint.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.Migrations
+{
+    /// <inheritdoc />
+    public partial class FiliereIdToint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Etudiants\" ALTER COLUMN \"FiliereId\" TYPE integer USING \"FiliereId\"::integer;");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Etudiants\" ALTER COLUMN \"FiliereId\" TYPE text USING \"FiliereId\"::text;");
+        }
+
+    }
+}
