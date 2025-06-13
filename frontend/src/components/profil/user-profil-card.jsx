5a83e0ac9b3c9816caeecfba2dedd183ed038dc8
ABDELOUAHED ABBAD <moabbad22@gmail.com>
2025-06-13 22:29:28 +0100

    lets see if it works

--- frontend/src/components/profil/user-profil-card.jsx
+++ frontend/src/components/profil/user-profil-card.jsx
@@ -92,13 +92,19 @@ export function UserProfileCard({ user,setUser }) {
             <CardContent className="flex flex-col items-center p-8">
                 {/* Photo de profil */}
                 <div className="relative mb-6 h-40 w-40 overflow-hidden rounded-full border-4 border-purple-100">
-                    <Image
-                        src={`${baseUrl}${user.profileImageUrl}`}
+                    <img
+                        src={
+                            user?.profileImageUrl
+                                ? `${baseUrl.replace(/\/$/, '')}/${user.profileImageUrl.replace(/^\//, '')}`
+                                : "/default-avatar.png"
+                        }
                         alt="User profile"
-                        width={160}
-                        height={160}
+                        width="160"
+                        height="160"
                         className="object-cover"
                     />
+
+
                     {/* Icône de caméra */}
                     <label
                         htmlFor="profileImageInput"
